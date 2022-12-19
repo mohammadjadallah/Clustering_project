62b79de0d8ca512e23165ec9a3893551e8058e65
unknown <mhmdjmala51@gmail.com>
2022-12-19 18:00:37 +0300

    Initial Commit

---
+++ venv/Lib/site-packages/jupyterlab_server/_version.py
@@ -0,0 +1,16 @@
+"""
+store the current version info of the server.
+
+"""
+import re
+
+__version__ = "2.16.5"
+
+# Build up version_info tuple for backwards compatibility
+pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
+match = re.match(pattern, __version__)
+assert match is not None  # noqa
+parts: list = [int(match[part]) for part in ["major", "minor", "patch"]]
+if match["rest"]:
+    parts.append(match["rest"])
+version_info = tuple(parts)
